738fa621b5b44f8f17770a988c481d8e770b08ec
Gelandi <mike.shimp@gmail.com>
2016-04-26 18:07:48 -0700

    First commit

---
+++ modules/polls/client/config/polls.client.config.js
@@ -0,0 +1,32 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('polls')
+    .run(menuConfig);
+
+  menuConfig.$inject = ['Menus'];
+
+  function menuConfig(Menus) {
+    // Set top bar menu items
+    Menus.addMenuItem('topbar', {
+      title: 'Polls',
+      state: 'polls',
+      type: 'dropdown',
+      roles: ['*']
+    });
+
+    // Add the dropdown list item
+    Menus.addSubMenuItem('topbar', 'polls', {
+      title: 'List Polls',
+      state: 'polls.list'
+    });
+
+    // Add the dropdown create item
+    Menus.addSubMenuItem('topbar', 'polls', {
+      title: 'Create Poll',
+      state: 'polls.create',
+      roles: ['user']
+    });
+  }
+})();
